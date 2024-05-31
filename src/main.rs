059a99d0234550e0e3759ed3591c2736be51d934
danielhe4rt <hey@danielheart.dev>
2024-05-31 18:13:48 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() {
-    let payload = std::fs::read("src/fixtures/img0.foo")
+    let payload = std::fs::read("src/fixtures/img2.foo")
         .unwrap()
         .iter()
         .map(|&x| x as char)
@@ -25,8 +25,29 @@ struct AsciiChar {
     ascii_value: u8,
     density: f64,
     brightness: u32,
-    index: usize,
-    converted_char: char,
+}
+
+impl AsciiChar {
+    pub fn new(ascii_byte: char) -> Self {
+        let ascii_byte_u32 = ascii_byte as u32;
+        let red = ((ascii_byte_u32 >> 16) & 0xff);
+        let green = ((ascii_byte_u32 >> 8) & 0xff);
+        let blue = (ascii_byte_u32) & 0xff;
+        let brightness = (red + green + blue) / 3;
+        let density = (brightness as f64 / 255.0);
+        Self {
+            raw_char: ascii_byte,
+            ascii_value: ascii_byte as u8,
+            density,
+            brightness,
+        }
+    }
+
+    pub fn convert(&self, table: &Vec<u8>, density_level: usize) -> char {
+        let ascii_index = ((table.len() / density_level) as f64) * self.density;
+
+        table.get(ascii_index as usize).unwrap().clone() as char
+    }
 }
 
 #[derive(Debug)]
@@ -41,6 +62,7 @@ impl AsciiConverter {
     pub fn new(payload: String) -> Self {
         let mut read_file = payload.split("\n").collect::<Vec<&str>>();
         let mut file_specs = read_file.remove(0).split(" ");
+
         let payload = read_file.join("\n");
         let width = file_specs.next().unwrap().parse::<i32>().unwrap();
         let height = file_specs.next().unwrap().parse::<i32>().unwrap();
@@ -54,54 +76,31 @@ impl AsciiConverter {
     pub fn convert(&self) {
         let mut char_map = fs::read("src/fixtures/char_map.fodase").unwrap();
         let char_map = char_map;
-        let ascii_list_length = char_map.len() as i32;
 
         for (i, char) in char_map.iter().enumerate() {
             println!("Index: {:?} | {:?}", i, char);
         }
 
-
         let foo_payload = self.payload
-            .as_bytes()
-            .chunks(3)
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(self.width as usize)
             .map(|v| v.to_vec())
-            .collect::<Vec<Vec<u8>>>()
+            .collect::<Vec<Vec<char>>>()
             .iter().map(|chunk| {
             chunk.iter()
                 .map(|&ascii_byte| {
-                    let blue = ((ascii_byte) & 0xff) as u32;
-                    let red = (ascii_byte as u32 >> 16u32) & 0xff;
-                    let green = (ascii_byte as u32 >> 8u32) & 0xff;
-                    let brightness = ((red + green + blue) / 3);
-                    let density = (brightness as f64 / 255.0) as f64;
-                    let ascii_index = ((ascii_list_length - 1) as f64 * density) as usize;
-                    let converted_char = char_map.get(ascii_index).unwrap().clone() as char;
-                    AsciiChar {
-                        raw_char: ascii_byte as char,
-                        ascii_value: ascii_byte,
-                        brightness,
-                        density,
-                        index: ascii_index,
-                        converted_char
-                    }
+                    AsciiChar::new(ascii_byte)
                 }).collect::<Vec<AsciiChar>>()
         }).collect::<Vec<Vec<AsciiChar>>>();
-
 
         let mut output = File::create("fodase.foo2").expect("deu ruim");
         for chunk in foo_payload {
             chunk.iter().for_each(|char| {
-                println!("{:?}", char);
-                write!(output, "{}", char.converted_char).unwrap()
+                write!(output, "{}", char.convert(&char_map, 4)).unwrap()
             });
             write!(output, "{}", "\n").unwrap();
-
         }
-
-
-        // for (i, char) in foo_filtered.into_iter().enumerate() {
-        //     println!("{i}, {char}");
-        // }
     }
 }
 
